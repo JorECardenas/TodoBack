20873f4c7495a6ec8573bd30540e66ba31a1e261
jorgencora <jorge.cardenas@encora.com>
2024-06-26 12:35:58 -0700

    Initial commit

---
+++ src/test/java/com/example/todoback/ToDoBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.todoback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
